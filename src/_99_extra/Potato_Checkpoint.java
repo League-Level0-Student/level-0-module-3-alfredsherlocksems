a61e91f958fe67eaf226ee4ebae409594477ba96
root <root@cd97eb3ce5b6>
2020-04-15 21:56:20 +0000

    yay

--- src/_99_extra/Potato_Checkpoint.java
+++ src/_99_extra/Potato_Checkpoint.java
@@ -5,11 +5,17 @@ public class Potato_Checkpoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
-	
-	
-	
-	
+	for (int i = 1; i < 9; i++) {
+if (i == 4) {
+	System.out.println(4);
+	}
+	else if (i == 8) {
+		System.out.println(" more ");
+	}
+	else {
+		System.out.println(i + " potato ");	
+	}
+	}                  
 	
 	
 	
